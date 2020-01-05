cb0497c145681ab93ef42ff26bc31daeaee48581
wenhaibo <1461154748@qq.com>
2020-01-05 17:22:17 +0800

    bottom

--- flutter_app_ios/lib/test/bottom_nav_bar.dart
+++ flutter_app_ios/lib/test/bottom_nav_bar.dart
@@ -1,42 +1,106 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app_ios/test/popmenu_button.dart';
 
-class MyBottomNavBar extends StatefulWidget{
+import 'home.dart';
+import 'list.dart';
+
+
+void main()=>runApp(MyBottomNavBar());
+class MyBottomNavBar extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
+    return MyBottombarState();
+  }
+}
+
+class MyBottombarState extends State<MyBottomNavBar> {
+  List<BottomNavigationBarItem> bottomebaritem = [];
+  List<Widget> _bottomPageList=List();
+  int _slecIndex=0;
 
 
-
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _bottomPageList..add(Home())
+    ..add(ExpansionTileSample())
+    ..add(PopMenuButton());
   }
 
 
-
-}
-
-class MyBottombarState extends State<MyBottomNavBar>{
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('底部导航测试'),
+        ),
+        body:PageView.builder(
+          controller: PageController(),
+          physics: NeverScrollableScrollPhysics(),
+          onPageChanged: _pageChanged,
+          itemCount: _bottomPageList.length,
+          itemBuilder: (context,index)=>_bottomPageList[index],
+           ),
 
-        ),
-        body: Center(
-          child: Text('这是页面一'),
-        ),
         floatingActionButton: FloatingActionButton(
-
+          child: Icon(Icons.add),
         ),
         bottomNavigationBar: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home,),
+              title: Text('主页')
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.skip_previous),
+              title: Text('使用')
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.verified_user),
+              title: Text('个人')
+            )
+          ],
+          type: BottomNavigationBarType.shifting,
+          onTap: onTabPaged,
+          selectedItemColor: Colors.lightBlueAccent,
+          unselectedItemColor: Colors.black,
+          showUnselectedLabels: true,
+          currentIndex: _slecIndex,
 
         ),
       ),
     );
+  }
 
+  void _pageChanged(int index){
+    setState(() {
+      _slecIndex=index;
+    });
 
 
   }
+  void onTabPaged(int index){
+    PageController().jumpToPage(index);
 
+  }
+  getBottomitemBar() {
+    return bottomebaritem
+      ..add(
+          BottomNavigationBarItem(
+            icon: Icon(Icons.forward),
+            title: Text('主页'),
+          ))
+      ..add(BottomNavigationBarItem(
+          icon: Icon(Icons.arrow_downward),
+          title: Text('个人')))
+      ..add(BottomNavigationBarItem(
+          icon: Icon(Icons.new_releases),
+          title: Text('活动')))
+      ..add(BottomNavigationBarItem(
+          icon: Icon(Icons.verified_user),
+          title: Text('商业')));
+  }
 }
---
+++ flutter_app_ios/lib/test/bottpage_test.dart
@@ -0,0 +1,89 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app_ios/test/expation_title.dart';
+import 'package:flutter_app_ios/test/popmenu_button.dart';
+
+import 'drop_down.dart';
+
+void main()=>runApp(HomeTest());
+class HomeTest extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _Home();
+  }
+
+}
+
+class _Home extends State<HomeTest> {
+  int _currentIndex = 0;
+  List<Widget> _pages;
+
+  @override
+  void initState() {
+    _pages = [
+      new Dropdown(),
+      new ExpationTitleTest(),
+      new PopMenuButton(),
+    ];
+    super.initState();
+
+
+  }
+
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
+  var _pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:  Scaffold(
+        body: PageView.builder(
+            controller: _pageController,
+            physics: NeverScrollableScrollPhysics(),
+            onPageChanged: _pageChanged,
+            itemCount: _pages.length,
+            itemBuilder: (context, index) => _pages[index]),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          fixedColor: Colors.black,//点击选择
+          type: BottomNavigationBarType.shifting,
+          onTap: onTabTapped,
+          items: [
+            BottomNavigationBarItem(
+              //backgroundColor: Theme.of(context).appBarTheme.color
+                icon: Icon(Icons.home),
+                title: Text("公会"),
+                backgroundColor: Theme.of(context).appBarTheme.color),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.notifications),
+                title: Text("活动"),
+                backgroundColor: Theme.of(context).appBarTheme.color),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                title: Text("我的"),
+                backgroundColor: Theme.of(context).appBarTheme.color),
+          ],
+        ),
+      ),
+    );
+
+  }
+
+  void _pageChanged(int index) {
+    setState(() {
+      if (_currentIndex != index) _currentIndex = index;
+    });
+  }
+
+  void onTabTapped(int index) {
+    _pageController.jumpToPage(index);
+  }
+}
